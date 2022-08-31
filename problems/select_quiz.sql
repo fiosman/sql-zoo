a82dc05bf9df790a25dca41b9dc8f70acd952752
fiosman <fares.osman@outlook.com>
2022-08-30 23:05:13 -0400

    Adds first query

---
+++ problems/select_quiz.sql
@@ -0,0 +1,7 @@
+-- Write query which produces the below table
+-- name	population
+-- Bahrain	1234571
+-- Swaziland	1220000
+-- Timor-Leste	1066409
+
+SELECT name, population FROM world WHERE population BETWEEN 1000000 AND 1250000;

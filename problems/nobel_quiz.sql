5126e06946e8de270f1e5df760b2f181327041fe
fiosman <fares.osman@outlook.com>
2022-09-06 23:25:39 -0400

    adds query

--- problems/nobel_quiz.sql
+++ problems/nobel_quiz.sql
@@ -2,3 +2,8 @@
 -- winners beginning with 'C' and ending in 'n'. 
 
 SELECT "winner" FROM "nobel" where "winner" LIKE 'C%' AND "winner" LIKE '%n';
+
+-- write a query that returns how many chemistry awards were given 
+-- between 1950 and 1960
+
+SELECT COUNT("subject") FROM "nobel" WHERE "subject" = 'Chemistry' AND "yr" BETWEEN 1950 AND 1960; 

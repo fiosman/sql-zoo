6997395ed6beb16c244f913d99784c7209189581
fiosman <fares.osman@outlook.com>
2022-09-07 23:51:14 -0400

    adds query

--- problems/nobel_quiz.sql
+++ problems/nobel_quiz.sql
@@ -7,3 +7,8 @@ SELECT "winner" FROM "nobel" where "winner" LIKE 'C%' AND "winner" LIKE '%n';
 -- between 1950 and 1960
 
 SELECT COUNT("subject") FROM "nobel" WHERE "subject" = 'Chemistry' AND "yr" BETWEEN 1950 AND 1960; 
+
+-- write a query that shows the amount of yrs where 
+-- no medicine awards were given
+
+SELECT COUNT("yr") FROM "nobel" WHERE "subject" != 'Medicine';
